e02fcc62ae64f4a3e065faefec7077933a038c32
Blake Irvin <blake.irvin@smartb.eu>
2015-10-26 17:17:11 +0100

    add script to prep ubuntu for docker

---
+++ files/prepare_ubuntu_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# basic setup for ubuntu
+dpkg-divert --local --rename --add /sbin/initctl
+ln -sf /bin/true /sbin/initctl
+apt-get update
+
+####################################################
+#### Customize your docker image here. For more ####
+#### inforation see https://docs.docker.com     ####
+####################################################
+
+# disable package installs
+apt-get clean && rm -rf /var/lib/apt/lists
